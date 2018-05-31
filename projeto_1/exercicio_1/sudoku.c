a39d8a1cf58232e9e07bc5b41a6573ec5c1656f2
bumbleblo <bumbleblo2013@gmail.com>
2018-05-31 16:24:32 -0300

    Finish cols and rows functions

--- projeto_1/exercicio_1/sudoku.c
+++ projeto_1/exercicio_1/sudoku.c
@@ -16,16 +16,6 @@ typedef struct MessageSubGrade{
 	int* boolean_result;
 }MessageSubGrade;
 
-void debug(){ // remove this function
-	int i,j; 
-	for(i=0; i<9; ++i){
-		for(j=0; j<9; ++j)
-			printf("[%2d]", sudoku[i][j]);
-		printf("\n");
-	}
-}
-
-
 void* sub_grade(void* message){
 	
 	MessageSubGrade* inputMessage;
@@ -49,13 +39,21 @@ void* sub_grade(void* message){
 	}
 
 	pthread_mutex_lock(&mutex);
-	 *inputMessage->boolean_result = boolean_result;
+	 *inputMessage->boolean_result = *inputMessage->boolean_result && boolean_result;
 	pthread_mutex_unlock(&mutex);
 
 
 }
 
-int confirm_col(int index){
+typedef struct MessageLine{
+	int line;
+	int * boolean_result;
+}MessageLine;
+
+void* confirm_col(void *message){
+
+	MessageLine *inputMessage;
+	inputMessage = (MessageLine*)message;
 
 	int conf[9];
 	int i;
@@ -64,14 +62,20 @@ int confirm_col(int index){
 	memset(conf, 0, sizeof(conf));
 
 	for(i=0; i<9; ++i)
-		conf[sudoku[i][index]-1]=1;
+		conf[sudoku[i][inputMessage->line]-1]=1;
 
 	for(i=0; i<9; ++i)
 		result = result && conf[i];
 
-	return result;
+	pthread_mutex_lock(&mutex);
+	*inputMessage->boolean_result = *inputMessage->boolean_result && result;
+	pthread_mutex_unlock(&mutex);
 }
-int confirm_row(int index){
+void* confirm_row(void* message){
+
+	MessageLine *inputMessage;
+	inputMessage = (MessageLine*)message;
+
 	int conf[9];
 	int i;
 	int result=1;
@@ -79,14 +83,16 @@ int confirm_row(int index){
 	memset(conf, 0, sizeof(conf));
 
 	for(i=0; i<9; ++i)
-		conf[sudoku[index][i]-1]=1;
+		conf[sudoku[inputMessage->line][i]-1]=1;
 
 	for(i=0; i<9; ++i){
 		result = result && conf[i];
 	}
 
+	pthread_mutex_lock(&mutex);
+	*inputMessage->boolean_result = *inputMessage->boolean_result && result;
+	pthread_mutex_unlock(&mutex);
 
-	return result;
 }
 
 int main(){
@@ -104,8 +110,9 @@ int main(){
 
 	int bool_cols=1, bool_rows=1, bool_cels=1;
 
+
 	pthread_t grades[9];
-	
+	int thread_cont=0;
 	for(i=1; i<9; i+=3){
 		for(j=1; j<9; j+=3){
 			MessageSubGrade message;
@@ -113,18 +120,39 @@ int main(){
 			message.y_axis = j;
 			message.boolean_result = &bool_cels;
 			
-			pthread_create(&grades[i], NULL, sub_grade, (void*)&message);
+			pthread_create(&grades[thread_cont++], NULL, sub_grade, (void*)&message);
 		}
 	}
+	
+
+	for(i=0; i<9; ++i)
+			pthread_join(grades[i], NULL);
+
+	
+	for(i=0; i<9; ++i){
+		MessageLine message;
+
+		message.line = i;
+		message.boolean_result = &bool_cels;
+		pthread_create(&grades[i], NULL, confirm_row,(void*)&message);
+	}
+
+	for(i=0; i<9; ++i)
+			pthread_join(grades[i], NULL);
 
 	for(i=0; i<9; ++i){
-		bool_rows = bool_rows && confirm_row(i);
-		bool_cols = bool_cols && confirm_col(i);
+		MessageLine message;
+		message.line = i;
+		message.boolean_result = &bool_cels;
+		pthread_create(&grades[i], NULL, confirm_col,(void*)&message);
 	}
+
+	for(i=0; i<9; ++i)
+			pthread_join(grades[i], NULL);
+
 
 	int result = bool_cels && bool_cols && bool_rows;
 
-	printf("[%d]\n", bool_cels);
 	if(result)
 		printf("YES");
 	else
