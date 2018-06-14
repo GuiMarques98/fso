1e529ec7e932f60b1f7e46a0fc9a7ce6f3bbb948
GuiMarques98 <guilherme.marques.moreira@gmail.com>
2018-06-14 10:23:02 -0300

    Adicionando arquivo

---
+++ projeto_2/gerenciador.c
@@ -0,0 +1,20 @@
+/*
+    Gerenciador de Memória Virtual
+
+Integrantes:
+    Guilherme Marques Moreira da Silva 16/0029503
+    Felipe Borges 16/0049733
+    Filipe Toyoshima Silva 16/0049971
+
+*/
+// INCLUDES
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+// MAIN
+int main(int argc, char const *argv[]) {
+    
+    return 0;
+}
